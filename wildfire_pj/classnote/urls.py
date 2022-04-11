cb581ac250a175f91f2f6bf1862b38998773e263
cooh24 <cooh24@gmail.com>
2022-04-11 11:13:24 +0900

    CBV로 블로그 포스트 상세 페이지 만들기

--- wildfire_pj/classnote/urls.py
+++ wildfire_pj/classnote/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),
+
+    path('<int:pk>/', views.PostDetail.as_view()),  #PostDetail 경로
+    # path('<int:pk>/', views.single_post_page),
+
+    path('', views.PostList.as_view()),  #views에 정의된 PostList를 view로 보여주는 경로
+    # path('', views.index),
 ]
--- wildfire_pj/classnote/views.py
+++ wildfire_pj/classnote/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
 
-    return render(request, 'classnote/index.html', {'posts':posts,})
+# Postlist를 ListView로 클래스생성 / index함수 대체
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    # template_name = 'classnote/index.html'    # 직접 지정
+    # ListView는 모델명뒤에 '_list'가 붙는 html파일을 기본템플릿으로 사용 / Post모델 : post_list
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
+# index 화면에 포스트모델의 객체를 최근생성순으로 불러오도록 함수생성
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
 
-    return render(request, 'classnote/single_post_page.html', {'post':post,})
+#     return render(request, 'classnote/index.html', {'posts':posts,})
+
+
+
+# post 상세페이지를 DetailView로 클래스생성 / single_post_page 함수 대체
+class PostDetail(DetailView):
+    model = Post
+    
+# single_post_page에 id=pk 값에 해당하는 포스트를 불러오는 함수 생성
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+
+#     return render(request, 'classnote/single_post_page.html', {'post':post,})
