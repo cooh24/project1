cb5bd72b72307ef9edfd8df9cf79039dbcf6a47b
cooh24 <cooh24@gmail.com>
2022-04-11 10:55:52 +0900

    대문 페이지와 자기소개 페이지 만들기

--- wildfire_pj/single_page/views.py
+++ wildfire_pj/single_page/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# landing.html 요청 시 불러오기
+def landing(request):
+    return render(request, 'single_page/landing.html',)
+
+# about_me.html 요청 시 불러오기
+def about_me(request):
+    return render(request, 'single_page/about_me.html',)
