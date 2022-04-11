c809ebed8af931fe1f8c5adeba0b88897e695156
cooh24 <cooh24@gmail.com>
2022-04-11 13:16:51 +0900

    첨부파일 있을 경우 포스트 상세페이지에 다운로드 버튼 출력하기

--- wildfire_pj/classnote/models.py
+++ wildfire_pj/classnote/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 # Create your models here.
 class Post(models.Model):
@@ -13,6 +14,14 @@ class Post(models.Model):
     # 포스트 생성시, '[id] title' 표현하도록 함
     def __str__(self):
         return f'[{self.pk}] {self.title}'
-
+    # post 주소
     def get_absolute_url(self):
         return f'/classnote/{self.pk}/'
+
+    # 업로드파일 이름 가져오는 함수
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    # 업로드파일 확장자 가져오는 함수
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
